490787b84cffe66d738de76d310caa3edad7da1f
James Bolton <james@neojames.me>
2020-04-02 14:38:22 +0100

    Writing sanity check test

---
+++ test/test_main.cpp
@@ -0,0 +1,39 @@
+// test_main.cpp
+// Runs all test cases
+// James Bolton (james@neojames.me)
+
+#include <Arduino.h>
+#include <unity.h>
+//Include additional tests
+
+// Setup
+
+void setUp(void) {
+
+}
+
+void tearDown(void) {
+
+}
+
+// Main Program Tests
+
+// // Test that enviroment is sane
+void test_led_builtin_pin_number(void) {
+    TEST_ASSERT_EQUAL(13, LED_BUILTIN);
+}
+
+void setup() {
+    // NOTE!!! Wait for >2 secs
+    // if board doesn't support software reset via Serial.DTR/RTS
+    delay(2000);
+
+    UNITY_BEGIN();    // IMPORTANT LINE!
+    RUN_TEST(test_led_builtin_pin_number);
+
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+
+}
