c32354f3c06264b1d5988a76db58905b4aaf27b1
James Bolton <james@neojames.me>
2020-04-02 16:19:33 +0100

    Guess i'll have to write tests differently...

--- test/test_main/test_main.cpp
+++ test/test_main/test_main.cpp
@@ -2,7 +2,6 @@
 // Runs all test cases
 // James Bolton (james@neojames.me)
 
-#include <Arduino.h>
 #include <unity.h>
 
 // Setup
@@ -18,19 +17,14 @@
 // Main Program Tests
 
 // // Test that enviroment is sane
-void test_led_builtin_pin_number(void) {
-    TEST_ASSERT_EQUAL(13, LED_BUILTIN);
-}
 
 void setup() {
     // NOTE!!! Wait for >2 secs
     // if board doesn't support software reset via Serial.DTR/RTS
     delay(2000);
-
-    UNITY_BEGIN();    // IMPORTANT LINE!
-    RUN_TEST(test_led_builtin_pin_number);
 }
 
 void loop() {
+    UNITY_BEGIN();
     UNITY_END(); // stop unit testing
 }
